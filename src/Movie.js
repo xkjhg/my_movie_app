8e097758cd427858abd34be1a12d0fac5438c964
xkjhg <xkjhg@naver.com>
2021-08-18 23:15:24 +0900

    5th commit

--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,22 @@
 import React from "react"
 import PropTypes from "prop-types"
+import "./Movie.css"
 
-function Movie({ id, year, title, summary, poster }) {
+function Movie({ year, title, summary, poster }) {
   return (
-    <div>
-      <h3>
-        {title}
-      </h3>
+    <div class="movie">
+      <img src={poster} alt={title} title={title} />
+      <div class="movie_data">
+        <h3 class="movie_title">
+          {title}
+        </h3>
+        <h5 class="movie_year">
+          {year}
+        </h5>
+        <p class="movie_summary">
+          {summary}
+        </p>
+      </div>
     </div>
   )
 }

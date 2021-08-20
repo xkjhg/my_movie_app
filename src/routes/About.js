5a93f3466db3269b7d0685a5770715cd2a2ec448
xkjhg <xkjhg@naver.com>
2021-08-20 23:31:01 +0900

    7th commit

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./About.css"
+
+function About() {
+  return (
+    <div className="about_container">
+      <span>About</span>
+    </div>
+  )
+}
+
+export default About

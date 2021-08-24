365331bf2c2104fc486b2d10fd15b24a989405e2
xkjhg <xkjhg@naver.com>
2021-08-24 21:55:29 +0900

    9th commit

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,10 +1,15 @@
 import React from "react"
 import "./About.css"
 
-function About() {
+function About(props) {
+  console.log(props)
   return (
     <div className="about_container">
-      <span>About</span>
+      <h3>My first React App - My movie app</h3>
+      <br />
+      <span>"Everything comes to him who hustles while he waits."</span>
+      <br />
+      <span>- Thomas A. Edison</span>
     </div>
   )
 }
